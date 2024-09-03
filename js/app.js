64047811f26965e178aa739238baed5fbef2abbf
⇨ Arman  ⇨ 21 y.o <59280987+rootpown@users.noreply.github.com>
2024-09-03 21:13:00 +0400

    исправление бурегра

--- js/app.js
+++ js/app.js
@@ -1,17 +1,6 @@
 const burger = document.getElementById("burger");
 const navMenu = document.getElementById("nav_menu");
 
-
-
-
 burger.addEventListener("click", () => {
-    if (navMenu.style.visibility === "visible") {
-        navMenu.style.visibility = "hidden";
-        navMenu.style.opacity = "0";
-    } else {
-        navMenu.style.visibility = "visible";
-        navMenu.style.opacity = "1";
-    }
-
-
-})
+    navMenu.classList.toggle("show");
+});
